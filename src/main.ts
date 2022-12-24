8011a9cfe0b712a528390f8ccbad6d647a1301f0
Mounir <polomnr1@gmail.com>
2022-12-24 11:00:34 +0100

    Done

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,33 @@
 const btn = document.getElementById('btn'),
-    menu = document.getElementById('menu');
+    menu = document.getElementById('menu'),
+    main = document.getElementsByTagName('main')[0],
+    buttons = document.querySelectorAll('.butt'),
+    cards = document.querySelectorAll('.card');
+
 
 btn?.addEventListener('click', () => {
     menu.classList.toggle('open')
 });
+
+main?.addEventListener('click', () => {
+    menu.classList.remove('open');
+})
+
+// @ts-ignore
+const removeActive = e => {
+    buttons.forEach(button => button.setAttribute('data-active', 'false'))
+    cards.forEach(card => {
+        if( card.getAttribute('data-index') === e.target.getAttribute('data-index')){
+            card.setAttribute('data-active', 'true')
+        }
+        else{
+            card.setAttribute('data-active', 'false')
+        }
+    })
+}
+
+buttons.forEach(button => button.addEventListener('click', e => {
+    removeActive(e);
+    // @ts-ignore
+    e.target.setAttribute('data-active', 'true')
+}))
